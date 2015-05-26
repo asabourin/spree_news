834c142835d89721e2c7127e5cf432a0bedebf97
Antoine Sabourin <antoine@redant.com.au>
2015-05-27 09:35:21 +1000

    wrapping scope in lambda as per activerecord 4.2 requirement

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -2,5 +2,5 @@ class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description
 
   scope :published, lambda { where(:published => true) }
-  scope :latest, order("created_at DESC").limit(3)
+  scope :latest, lambda { order("created_at DESC").limit(3) }
 end
